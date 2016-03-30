f121ffc8cced187b5604397e925ec6acd27b74b9
trashctor <trashctor1@gmail.com>
2016-03-30 10:07:49 -0400

    Create User model.

---
+++ app/models/user.rb
@@ -0,0 +1,48 @@
+class User < ActiveRecord::Base
+	attr_reader :password
+
+	validates :username, :password_digest, :session_token, presence: true
+	validates :username, format: { with: /\A[A-Za-z][A-Za-z0-9]*$\z/ }
+	validates :password, length: { minimum: 6 }
+
+	after_initialize :ensure_session_token
+	after_initialize :ensure_dummy_password
+
+	def self.find_by_credentials(username, password)
+		user = User.find_by(username: username)
+
+		return nil unless user
+		user.is_password?(password) ? user : nil
+	end
+
+	def password=(password)
+		@password = password
+		self.password_digest = BCrypt::Password.create(self.password).to_s
+	end
+
+	def is_password?(password)
+		BCrypt::Password.new(self.password_digest).is_password?(password)
+	end
+
+	def reset_token!
+		self.session_token = SecureRandom::urlsafe_base64
+
+		while	User.exists?(session_token: self.session_token)
+			self.session_token = SecureRandom::urlsafe_base64
+		end
+
+		self.save!
+		self.session_token
+	end
+
+private
+
+	def ensure_session_token
+		self.session_token = SecureRandom::urlsafe_base64 unless self.session_token
+	end
+
+	def ensure_dummy_password
+		# allows a User fetched from the database to be saved (pass validation)
+		@password = "123456" unless self.password
+	end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160329150719) do
+ActiveRecord::Schema.define(version: 20160330130543) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "users", force: :cascade do |t|
+    t.string   "username",        null: false
+    t.string   "password_digest", null: false
+    t.string   "session_token",   null: false
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+  end
 
   create_table "videos", force: :cascade do |t|
     t.string   "title",       null: false
