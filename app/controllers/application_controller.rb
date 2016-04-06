0c0babcefdad3bfd48c3b36849f94fd8a302237c
trashctor <trashctor1@gmail.com>
2016-04-06 18:52:19 -0400

    Enable CSRF protection for production only.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,9 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  # protect_from_forgery with: :exception
+	if Rails.env.production?
+  	protect_from_forgery with: :exception
+	end
 
 	def current_user
 		@current_user ||= User.find_by(session_token: session[:session_token])
